8004ad1d53466aced5848f6c6af6fd5ee725658f
Scott Wilson <scott@Scotts-MacBook-Pro.local>
2024-01-02 07:25:40 -0600

    day 1 project - band name generator

---
+++ day-001/main.py
@@ -0,0 +1,13 @@
+#1 Create a greeting for your program.
+print("\r\nWelcome to the Band Name Generator")
+
+#2. Ask the user for the city that they grew up in.
+city_name = input("\r\nWhat city did you grow up in?\r\n")
+
+#3. Ask the user for the name of a pet.
+pet_name = input("\r\nWhat is the name of your pet?\r\n")
+
+#4. Combine the name of their city and pet and then show them their bane name.
+print("\r\nThe name of your band is " + city_name + " " + pet_name + ".\r\n");
+
+#5. Make sure the input cursor shows up on a new line, ...
